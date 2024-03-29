6e36dbb3ddfc536aa553c69b2c165db8f0dcd888
KevinTheDude511 <d.khuong0511@gmail.com>
2024-03-29 15:52:23 +0700

    adjust created_at format

--- back-end/controllers/ActivityController.js
+++ back-end/controllers/ActivityController.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+moment().utcOffset('+07:00');
 const Activity = require(`../models/ActivityModel`)
 
 const readOnOff = async (req, res) => {
@@ -6,7 +8,13 @@ const readOnOff = async (req, res) => {
         const onOffActivities = await Activity.find({})
             .sort({created_at: -1})
             .limit(parseInt(limit));
-        res.status(200).json({data: onOffActivities})
+
+        const formattedActivities = onOffActivities.map(activity => ({
+            ...activity.toObject(),
+            created_at: moment(activity.created_at).format('YYYY-MM-DD HH:mm:ss')
+        }));
+
+        res.status(200).json({data: formattedActivities})
     } catch (error) {
         console.log(error)
         res.status(500).json({ msg: error })
