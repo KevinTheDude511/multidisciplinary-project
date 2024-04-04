cb4fb122ecbbf2cbec81e5f4bdc46ce2553af56e
Đỗ Duy Khương <d.khuong0511@gmail.com>
2024-04-04 20:14:40 +0700

    add Factory Method design pattern

--- back-end/controllers/ActivityController.js
+++ back-end/controllers/ActivityController.js
@@ -1,5 +1,13 @@
 const Activity = require(`../models/ActivityModel`)
 
+const removePrefix = (activities) => {
+    const modifiedActivities = activities.map(activity => {
+        activity.feed_id = activity.feed_id.replace(/^smarthome\./, '');
+        return activity;
+    });
+    return modifiedActivities
+}
+
 const readOnOff = async (req, res) => {
     try {
         const {limit} = req.params
@@ -8,7 +16,19 @@ const readOnOff = async (req, res) => {
         })
             .sort({created_at: -1})
             .limit(parseInt(limit));
-        res.status(200).json({data: onOffActivities})
+        const modifiedActivities = removePrefix(onOffActivities)
+        res.status(200).json({data: modifiedActivities})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ msg: error })
+    }
+}
+
+const readAll = async (req, res) => {
+    try {
+        const allActivities = await Activity.find({}).sort({created_at: -1})
+        const modifiedActivities = removePrefix(allActivities)
+        res.status(200).json({data: modifiedActivities})
     } catch (error) {
         console.log(error)
         res.status(500).json({ msg: error })
@@ -17,4 +37,5 @@ const readOnOff = async (req, res) => {
 
 module.exports = {
     readOnOff,
+    readAll,
 }
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const dynamicController = require('../controllers/DynamicController')
+const controllerFactory = require('../controllers/ControllerFactory')
 const activityController = require('../controllers/ActivityController')
 
 const collections = ['fan', 'humisensor', 'led', 'lightsensor', 'tempsensor', 'test']
 
 collections.forEach((collection) => {
-    controller = dynamicController(collection)
+    const controller = controllerFactory(collection)
     router.route(`/halfday/${collection}`)
         .get(controller.readAveragePerHourForHalfDay)
         .post(controller.createData)
     router.route(`/week/${collection}`)
         .get(controller.readAveragePerDayForWeek)
 })
-
+router.route(`/onoff/activity`)
+    .get(activityController.readAll)
 router.route(`/onoff/activity/:limit`)
     .get(activityController.readOnOff)
 
